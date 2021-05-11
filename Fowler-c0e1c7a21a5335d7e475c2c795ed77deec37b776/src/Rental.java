d64deaceecb895144916fd3af5efb1632d954ced
Eightdays <kevin.noisternig@web.de>
2021-05-11 23:32:45 +0200

    Replace Parameter with Method Call in Rental Class

--- Fowler-c0e1c7a21a5335d7e475c2c795ed77deec37b776/src/Rental.java
+++ Fowler-c0e1c7a21a5335d7e475c2c795ed77deec37b776/src/Rental.java
@@ -1,10 +1,12 @@
 class Rental {
     private Movie movie;
     private int daysRented;
-    public Rental(Movie newmovie, int newdaysRented) {
-        movie = newmovie;
-        daysRented = newdaysRented;
+    public Rental(Movie newMovie, int newDaysRented) {
+        this.setNewMovie(newMovie);
+        this.setDaysRented(newDaysRented);
     }
+    private void setNewMovie(Movie newMovie) {movie = newMovie;}
+    private void setDaysRented(int newDaysRented) {daysRented = newDaysRented;}
     public int getDaysRented() {
         return daysRented;
     }
